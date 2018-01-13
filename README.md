57e12c56a53790cc302ac0e71cda5ba3043ca8d5
Eric J <34559118+jiapurdue@users.noreply.github.com>
2018-01-13 18:18:08 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -4,8 +4,13 @@ About:
 
 This project is to create a Movie Trailer Website with list of movies definied in the entertaiment_center.py 
 
+Prerequiremnt:
+You must install Python. 
 
 How to Run:
-
-clone this file directly to local and run entertainment_center.py which will automatically open the website in your defaul browser.
+1: clone this file directly to local repository or just simply download the code
+2: make sure all three py file are in the same folder.
+3: run entertainment_center.py
+4: your defaul browser shall automatically open the Movie Trailer Website.
+5: Enjoy!
 
--- media.py
+++ media.py
@@ -2,13 +2,15 @@ import webbrowser
 
 
 class Movie(object):
+    """ This class is to create a Movie class """
 
     def __init__(self, mv_title, mv_storyline, mv_poster_image, mv_trailer):
-        # Create Movie object structure
+        """ Create Movie object structure """
         self.title = mv_title
         self.storyline = mv_storyline
         self.poster_image_url = mv_poster_image
         self.trailer_youtube_url = mv_trailer
 
     def show_trailer(self):
+        """ This function is to open a webbrowser and play the video """
         webbrowser.open(self.trailer_youtube_url)
